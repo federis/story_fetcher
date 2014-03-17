01767a5c1a3b3dac9da5f2a2ac0efef7eebfa6e8
Ben Roesch <bcroesch@gmail.com>
2014-03-17 17:54:13 -0500

    Switch to feedjira

--- lib/story_fetcher.rb
+++ lib/story_fetcher.rb
@@ -4,8 +4,8 @@ require "feedzirra"
 module StoryFetcher
   def self.fetch_all_feeds
     StoryFetcher::Feed.all.each do |feed|
-      fetched_feed = Feedzirra::Feed.fetch_and_parse(feed.url)
-      if fetched_feed.is_a?(Feedzirra::Parser::RSS)
+      fetched_feed = Feedjira::Feed.fetch_and_parse(feed.url)
+      if fetched_feed.is_a?(Feedjira::Parser::RSS)
         yield feed, fetched_feed.entries 
       end
     end
